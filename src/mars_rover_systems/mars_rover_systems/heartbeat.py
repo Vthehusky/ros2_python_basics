681bc0ee60350c8851d69ddfb17d44ed27829694
Vinay Prakash <vinaypra@mtu.edu>
2025-08-29 02:03:37 +0000

    Added multiple heartbeat diagnostic messages in entry point in setup.py to run as executables

--- src/mars_rover_systems/mars_rover_systems/heartbeat.py
+++ src/mars_rover_systems/mars_rover_systems/heartbeat.py
@@ -7,5 +7,10 @@ def main(args=None):
     print("Mars rover 1 is alive...")
     rclpy.shutdown()
 
+def main_shutdown(args=None):
+    rclpy.init(args=args)
+    print("Shutting down Mars rover 1...")
+    rclpy.shutdown()
+
 if __name__== '__main__':
     main()
