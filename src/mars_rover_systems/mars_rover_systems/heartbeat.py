729c0019e61c8951596f463c039a072eb0997785
Vinay Prakash <vinaypra@mtu.edu>
2025-09-10 01:41:01 +0000

    Playing around with beginner style ROS2 node(s)

--- src/mars_rover_systems/mars_rover_systems/heartbeat.py
+++ src/mars_rover_systems/mars_rover_systems/heartbeat.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
 import rclpy
+import time
+from rclpy.node import Node
 
 def main(args=None):
     rclpy.init(args=args)
-    print("Mars rover 1 is alive...")
+
+    node = Node('mars_rover_1')
+
+    i = 0
+    max_i = 50
+    while i < max_i:
+        i += 1
+        ros_time_stamp = node.get_clock().now()
+        #time_stamp = time.time()
+        node.get_logger().info(str(i)+" Mars rover 1 is alive..."+str(ros_time_stamp))
+        #print(str(i)+"Mars rover 1 is alive..."+str(time_stamp))
+        time.sleep(1)
+
+    #print("Mars rover 1 is alive...")
+    node.destroy_node()
     rclpy.shutdown()
 
 def main_shutdown(args=None):
